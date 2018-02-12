e4ab6956946e75a98fc3e93d7ffe47d85eb9de73
cubamex209 <omarh209@gmail.com>
2018-02-11 19:27:57 -0800

    formatting html,Css

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 var sanity = "its running"
 console.log(sanity); // Sanity Check
 
-var aboutme = (".aboutme");
-var win = (window);
-
-win.on("scroll", function (){
-  console.log("scrolling");
-});
+document.getElementById("form").submit();
